fd09ca7044d59dfe2dd4aed03eff24c9f96643c8
Lazar Slavkovic-Raco <lslavkov@redhat.com>
2020-03-23 08:18:57 +0100

    Creating database creation

---
+++ Med-App-API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using Med_App_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Med_App_API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<User> User { get; set; }
+        public DbSet<Appointments> Appointments { get; set; }
+        public DbSet<Patient> Patient { get; set; }
+        public DbSet<Physician> Physician { get; set; }
+    }
+}
