00567eeff2f61e9fe2b5575149cf67c29e00f691
Lazar Slavkovic-Raco <lslavkov@redhat.com>
2020-04-03 18:06:28 +0200

    Register form

--- Med-App-SPA/src/app/register/register.component.ts
+++ Med-App-SPA/src/app/register/register.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {User} from "../_models/user";
+import {AuthService} from "../_service/auth.service";
+import {Physician} from "../_models/physician";
+import {Patient} from "../_models/patient";
 
 @Component({
   selector: 'app-register',
@@ -6,11 +11,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  registerForm: any;
+  @Output() cancelRegister = new EventEmitter();
+  registerForm: FormGroup;
+  user: User;
+  patient: Patient;
+  physician: Physician;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder, private authService: AuthService) {
   }
 
+  ngOnInit(): void {
+    this.createRegisterForm();
+  }
+
+  createRegisterForm() {
+    this.registerForm = this.fb.group({
+        type: ['patient'],
+        firstName: ['', Validators.required],
+        lastName: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['',
+          [
+            Validators.required,
+            Validators.minLength(4),
+            Validators.maxLength(8)
+          ]
+        ],
+        confirmPassword: ['', Validators.required],
+      }, {validators: this.passwordMatchValidator}
+    );
+  }
+
+  passwordMatchValidator(g: FormGroup) {
+    return g.get('password').value === g.get('confirmPassword').value
+      ? null
+      : {mismatch: true};
+  }
+
+  register() {
+    if (this.registerForm.valid) {
+      this.user = Object.assign({}, this.registerForm.value);
+      if (this.user['type'] === 'patient') {
+        this.patient = Object.assign({}, this.registerForm.value);
+        this.authService.registerPatient(this.patient).subscribe(() => {
+          console.log('Registration succesfull as patient')
+        }, error => {
+          console.log(error);
+        })
+      } else if (this.user['type'] === 'physician') {
+        this.physician = Object.assign({}, this.registerForm.value);
+        this.authService.registerPhysician(this.physician).subscribe(() => {
+          console.log('Registration succesfull as physician')
+        }, error => {
+          console.log(error);
+        })
+      }
+    }
+  }
+
+
+  cancel() {
+    this.cancelRegister.emit(false);
+  }
 }
