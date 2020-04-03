22d34c23185deed4e46c603c46066bc818d52974
Lazar Slavkovic-Raco <lslavkov@redhat.com>
2020-04-03 18:06:18 +0200

    Service for auth

--- Med-App-SPA/src/app/_service/auth.service.ts
+++ Med-App-SPA/src/app/_service/auth.service.ts
@@ -1,9 +1,27 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+// import {JwtHelperService} from '@auth0/angular-jwt'
+import {Patient} from "../_models/patient";
+import {Physician} from "../_models/physician";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  baseUrl = 'http:localhost:5000/api/auth/';
+  // jwtHelper = new JwtHelperService();
+  decodedToken: any;
+  currentPatient: Patient;
+  currentPhysician: Physician;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  registerPatient(patient: Patient) {
+    return this.http.post(this.baseUrl + 'register/patient', patient);
+  }
+  registerPhysician(physician: Physician) {
+    return this.http.post(this.baseUrl + 'register/physician', physician);
+  }
 }
