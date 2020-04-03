a3e527b5b1384ff3a072c411dd7f29f64f843a24
Lazar Slavkovic-Raco <lslavkov@redhat.com>
2020-04-03 18:06:50 +0200

    Imported some new module

--- Med-App-SPA/src/app/app.module.ts
+++ Med-App-SPA/src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {HttpClientModule} from "@angular/common/http";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import { RegisterComponent } from './register/register.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
+// import {JwtModule} from '@auth0/angular-jwt'
+
+import {AppComponent} from './app.component';
+import {NavComponent} from './nav/nav.component';
+import {RegisterComponent} from './register/register.component';
+import {HomeComponent} from './home/home.component';
+import {LoginComponent} from './login/login.component';
 import {RouterModule} from "@angular/router";
 import {appRoutes} from "../routes";
-import { FooterComponent } from './footer/footer.component';
+import {FooterComponent} from './footer/footer.component';
+
+export function tokenGetter() {
+  return localStorage.getItem('token');
+}
 
 @NgModule({
   declarations: [
@@ -26,9 +31,18 @@ import { FooterComponent } from './footer/footer.component';
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot(appRoutes),
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule,
+    // JwtModule.forRoot({
+    //   config: {
+    //     tokenGetter,
+    //     whitelistedDomains: ['localhost:5000'],
+    //     blacklistedRoutes: ['localhost:5000/api/auth']
+    //   }
+    // })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
