9315e4e8c84df0263a0ba9f79d7128d0549dd2d7
Lazar Slavkovic-Raco <lslavkov@redhat.com>
2020-04-08 17:53:35 +0200

    Config DbContext

--- Med-App-API/Data/DataContext.cs
+++ Med-App-API/Data/DataContext.cs
@@ -6,7 +6,7 @@ namespace Med_App_API.Data
     public class DataContext : DbContext
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
-        public DbSet<User> User { get; set; }
+        // public DbSet<User> User { get; set; }
         public DbSet<Physician> Physician { get; set; }
         public DbSet<Patient> Patient { get; set; }
         public DbSet<Workday> Workday { get; set; }
