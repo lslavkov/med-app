81cc2660a4a2fc921d25dfa0645f84e503739673
Lazar Slavkovic-Raco <lslavkov@redhat.com>
2020-05-21 18:54:30 +0200

    Register based on email service
    Closes #10

--- Med-App-API/Dto/UserForRegisterDto.cs
+++ Med-App-API/Dto/UserForRegisterDto.cs
@@ -7,11 +7,12 @@ namespace Med_App_API.Dto
         [Required] public string FirstName { get; set; }
         [Required] public string LastName { get; set; }
         [Required] [EmailAddress] public string Email { get; set; }
-        [Required] public string UserName { get; set; }
+
+        [StringLength(6, MinimumLength = 6, ErrorMessage = "Username has to be 6 characters")]
+        public string UserName { get; set; }
 
         [Required]
         [StringLength(16, MinimumLength = 8, ErrorMessage = "You must specify between 8 and 16")]
         public string Password { get; set; }
-
     }
 }
