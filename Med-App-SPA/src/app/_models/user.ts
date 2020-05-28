8bfc846a09bc0d6fe6ac1e2c9913599fa797dda6
Lazar Slavkovic-Raco <lslavkov@redhat.com>
2020-05-28 18:14:56 +0200

    WIP: User profile

--- Med-App-SPA/src/app/_models/user.ts
+++ Med-App-SPA/src/app/_models/user.ts
@@ -3,6 +3,4 @@ export interface User {
   email:string;
   first_name:string;
   last_name:string;
-  username:string;
-  role?: string;
 }
--- Med-App-SPA/src/app/user/user-edit/user-edit.component.ts
+++ Med-App-SPA/src/app/user/user-edit/user-edit.component.ts
@@ -11,20 +11,16 @@ import {NgForm} from "@angular/forms";
 export class UserEditComponent implements OnInit {
   @ViewChild('editForm', { static: true }) editForm: NgForm;
   user: User;
-  @HostListener('window:beforeunload', ['$event'])
-  unloadNotification($event: any) {
-    if (this.editForm.dirty) {
-      $event.returnValue = true;
-    }
-  }
 
   constructor(private activatedRoute: ActivatedRoute) {
+    this.activatedRoute.data.subscribe(data=>{
+      this.user = data['user'];
+      console.log(data);
+      console.log(this.user);
+    })
   }
 
   ngOnInit() {
-    this.activatedRoute.data.subscribe(data => {
-      this.user = data['user'];
-    });
   }
 
   updateUser() {
